48833a52ef66e576953efdc2b303d99c8acd2c53
aashutosh-adk <aashutoshadhikari82@gmail.com>
2026-06-23 17:07:57 +0545

    removed unnecessary files

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
 
 import Hero from './Components/Hero'
 import Feature from './Components/Feature'
-import Pricing from './Components/Pricing'
 import Navbar from './Components/Navbar'
-import Testimonials from './Components/Testimonials.jsx'
 import Footer from './Components/Footer'
 
 function App() {
@@ -12,9 +10,7 @@ function App() {
      <div className="min-h-screen bg-slate-950 overflow-hidden">
       <Hero/>
       <Feature/>
-      <Pricing/>
       <Navbar/>
-      <Testimonials/>
       <Footer/>
       </div>   
      </>
